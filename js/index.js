45e769409c0a70d53d6abce80886dfe9921f9dee
JH <vvaa277@gmail.com>
2023-03-07 22:17:52 +0900

    Feat : 모달창 html, css 추가
    input-bar-wrap과 형제 위치에 .modal-bg 로 추가, 현재 js는 추가 하지 않았고 class .hidden을 통해 숨겨놨다

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const $ = (selector) => document.querySelector(selector);
+const $All = (selector) => document.querySelectorAll(selector);
+
+const tabButtons = document.querySelectorAll(".tab-button");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabButtons.forEach((tab, index) => {
+  tab.addEventListener("click", function () {
+    tabContents.forEach((inner) => {
+      inner.classList.remove("active");
+    });
+
+    tabButtons.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    tabButtons[index].classList.add("active");
+    tabContents[index].classList.add("active");
+  });
+});
