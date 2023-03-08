29a1411a94b8cff4e9450b174c0cac8909635f37
JH <vvaa277@gmail.com>
2023-03-09 00:07:56 +0900

    Feat: chart-board영역을 누르면 하단 영역을 여는 기능 추가
    chart-board 내부의 상세 영역을 클릭해야 열려야 하지만 임시로 chart-board영역을 누르면 열리도록 해놓았다

--- js/index.js
+++ js/index.js
@@ -19,5 +19,15 @@ tabButtons.forEach((tab, index) => {
   });
 });
 
+const chartBoard = document.querySelector(".chart-board");
+// 각 카테고리 항목으로 변경해야함
+const chartHidden = document.querySelectorAll(".hidden_area");
+
+// chartBoard와 chartHidden영역이 아닌곳을 클릭하면 다시 닫기?
+chartBoard.addEventListener("click", function(){
+  chartHidden.forEach(element => {
+    element.classList.remove("hidden")
+  });
+})
 
 
