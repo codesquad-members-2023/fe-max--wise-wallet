31aac18645bc239e43aea3afd35597e6aeac2c8d
Daeram Chung <daeram.chung@gmail.com>
2023-03-10 04:49:20 +0900

    Add new payment method modal behaviors
    - Open/close modal.
    - Add new payment method to list of payment methods upon submission.

--- src/js/input-bar/dropdown.js
+++ src/js/input-bar/dropdown.js
@@ -1,10 +1,13 @@
-//- Dropdowns - payment method and category
+//- Dropdowns - Payment Method and Category
 const paymentMethodDropdown = document.querySelector(
   "#payment-method-dropdown"
 );
 const paymentMethodOptionsContainer = paymentMethodDropdown.nextElementSibling;
 const paymentMethodOptions =
   paymentMethodOptionsContainer.querySelectorAll(".option");
+const addPaymentMethodOption = paymentMethodOptionsContainer.querySelector(
+  "#add-payment-method-option"
+);
 const paymentMethodInputDisplay = paymentMethodDropdown.querySelector(
   ".input-bar__item-input"
 );
@@ -15,10 +18,18 @@ const categoryInputDisplay = categoryDropdown.querySelector(
   ".input-bar__item-input"
 );
 
-//- Close dropdown when clicked outside.
+//- New Payment Method Modal
+const newPaymentMethodModal = document.querySelector(
+  ".new-payment-method-modal"
+);
+const newPaymentMethodForm = newPaymentMethodModal.querySelector("form");
+const modalCancelBtn = newPaymentMethodModal.querySelector(".cancel-btn");
+
+//- Click Outside to Close
 window.addEventListener("click", (evt) => {
   const el = evt.target;
 
+  //- Close dropdowns
   if (
     !paymentMethodDropdown.contains(el) &&
     !paymentMethodOptionsContainer.contains(el) &&
@@ -29,6 +40,11 @@ window.addEventListener("click", (evt) => {
   ) {
     paymentMethodOptionsContainer.classList.remove("is-active");
     categoryOptionsContainer.classList.remove("is-active");
+  }
+
+  //- Close modal
+  if (el === newPaymentMethodModal) {
+    newPaymentMethodModal.classList.remove("is-active");
   }
 });
 
@@ -50,11 +66,11 @@ paymentMethodOptions.forEach((opt) => {
 
     const selectedOption = opt.dataset.value;
 
-    //- Add Payment Method
+    //- Open Payment Method Modal
     if (selectedOption === "추가하기") {
       paymentMethodInputDisplay.innerText = "선택하세요";
       paymentMethodInputDisplay.style.color = "var(--color-primary-alt)";
-      console.log("modal!"); // open up modal!!!!!!!!!
+      newPaymentMethodModal.classList.add("is-active");
       return;
     }
 
@@ -79,4 +95,38 @@ categoryOptions.forEach((opt) => {
     categoryInputDisplay.style.color = "var(--color-primary)";
     categoryOptionsContainer.classList.remove("is-active");
   });
+});
+
+//- Add New Payment Method
+newPaymentMethodForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const newPaymentMethod =
+    newPaymentMethodForm["new-payment-method"].value.trim();
+
+  const newLi = document.createElement("li");
+  newLi.classList.add("option");
+  newLi.dataset.value = newPaymentMethod;
+  newLi.innerHTML = `
+    <span>${newPaymentMethod}</span>
+    <button type="button" class="option-delete-btn">
+      <img
+        src="src/assets/icons/close.svg"
+        alt="Remove Payment Method" />
+    </button>
+  `;
+  newLi.addEventListener("click", (evt) => {
+    const deleteBtn = newLi.querySelector(".option-delete-btn");
+    if (deleteBtn.contains(evt.target)) {
+      newLi.remove();
+    }
+  });
+
+  paymentMethodOptionsContainer.insertBefore(newLi, addPaymentMethodOption);
+
+  newPaymentMethodModal.classList.remove("is-active");
+});
+
+//- Close Modal
+modalCancelBtn.addEventListener("click", () => {
+  newPaymentMethodModal.classList.remove("is-active");
 });
