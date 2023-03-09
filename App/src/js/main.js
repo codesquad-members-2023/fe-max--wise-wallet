bea5ec5fa7a27501a0e5860020a2220ca0027e40
jay <chlwodnjs34@gmail.com>
2023-03-10 00:55:02 +0900

    [refact] input bar +/- 버튼 수정, main 전체 내역 check box 수정

--- App/src/js/main.js
+++ App/src/js/main.js
@@ -20,9 +20,59 @@ const setDate = (date_element, date) => {
   month_en.innerHTML = month_en_Text;
 };
 
+const change_ul = () => {
+  const expenditure_li = `<li class="option body-regular">
+<span class="option">생활</span>
+</li>
+<li class="option body-regular">
+<span class="option">식비</span>
+</li>
+<li class="option body-regular">
+<span class="option">교통</span>
+</li>
+<li class="option body-regular">
+<span class="option">쇼핑/뷰티</span>
+</li>
+<li class="option body-regular">
+<span class="option">의료/건강</span>
+</li>
+<li class="option body-regular">
+<span class="">문화/여가</span>
+</li>
+<li class="option body-regular">
+<span class="option">미분류</span>
+</li>`;
+
+  const income_li = `<li class="option body-regular">
+<span class="option">월급</span>
+</li>
+<li class="option body-regular">
+<span class="option">용돈</span>
+</li>
+<li class="option body-regular">
+<span class="option">기타수입</span>
+</li>`;
+
+  const select = document.getElementById("select_div");
+  const checked = document.getElementById("price_toggle").checked;
+  const drop_menu = document.getElementById("drop_menu");
+  const new_ul = document.createElement("ul");
+  new_ul.id = "drop_menu";
+
+  checked
+    ? (new_ul.innerHTML = expenditure_li)
+    : (new_ul.innerHTML = income_li);
+  if (drop_menu !== null) {
+    select.removeChild(drop_menu);
+  }
+  select.appendChild(new_ul);
+};
+
 const init = () => {
   const left_arrow = document.getElementById("left-Arrow");
   const right_arrow = document.getElementById("right-Arrow");
+  const price_checkbox = document.getElementById("price_checkbox");
+
   const input_bar_date = left_arrow.addEventListener("click", () => {
     const [year, month, month_en] = get_date_element();
     const newDate = new Date(year.innerHTML, month.innerHTML - 2);
@@ -34,6 +84,8 @@ const init = () => {
     const newDate = new Date(year.innerHTML, month.innerHTML);
     setDate([year, month, month_en], newDate);
   });
+
+  price_checkbox.addEventListener("click", change_ul);
 
   // set Date Now
   setDate(get_date_element(), new Date());
