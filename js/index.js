2f13f43228aa3a7d4501a99363bc1be11af26619
JH <vvaa277@gmail.com>
2023-03-10 14:43:13 +0900

    Style: js 주석달린ver

--- js/index.js
+++ js/index.js
@@ -1,23 +1,33 @@
 const $ = (selector) => document.querySelector(selector);
 const $All = (selector) => document.querySelectorAll(selector);
 
+/* 파일분리 생각해두기 */
+
 const tabButtons = document.querySelectorAll(".tab-button");
 const tabContents = document.querySelectorAll(".tab-content");
+
 const buttonPaths = document.querySelectorAll(".tab-button path");
 
-tabButtons.forEach((tab, index) => {
-  tab.addEventListener("click", function (event) {
-    tabContents.forEach((inner) => {
-      inner.classList.remove("active");
+console.log(tabButtons)
+console.log(tabContents)
+
+tabButtons.forEach((button, index) => {
+  //탭버튼 모음 buttons = [button,button,button] 
+  // querySelectorAll = ".tab-button" 이 선택자(#이면 아이디, . 클래스) 를 가진 모든 요소의 배열[]
+  // querySelector = ".tab-button"을 가진 모든 요소의 첫번째 = buttons[0]
+
+  button.addEventListener("click", function (event) {
+    //button하나 클릭시
+    tabContents.forEach((tab) => {
+      //탭 영역 모음 tabContents = [tab,tab,tab]
+      tab.classList.remove("active");
+      //모든 탭의 .acitve를 떼어줌(기본속성으로 세팅된 display: none으로 인해 감춰짐)
     });
 
-    tabButtons.forEach((item) => {
-      item.classList.remove("active");
-    });
-
-    tabButtons[index].classList.add("active");
     tabContents[index].classList.add("active");
+     //해당 탭에(=[index]) .acitve를 붙여주어 영역 보여주기(display: block or flex 등 none이아니면 됨)
 
+    /* 버튼 색상 */
     buttonPaths.forEach((path) => {
       path.setAttribute("stroke", "#A79FCB");
     });
@@ -31,6 +41,7 @@ tabButtons.forEach((tab, index) => {
   });
 });
 
+/* 통계탭 열기 */
 const chartBoard = document.querySelector(".chart-board");
 // 각 카테고리 항목으로 변경해야함
 const chartHidden = document.querySelectorAll(".hidden_area");
@@ -38,7 +49,7 @@ const chartHidden = document.querySelectorAll(".hidden_area");
 // chartBoard와 chartHidden영역이 아닌곳을 클릭하면 다시 닫기?
 chartBoard.addEventListener("click", function () {
   chartHidden.forEach((element) => {
-    element.classList.remove("hidden");
+    element.classList.remove("hidden"); 
   });
 });
 
@@ -67,3 +78,11 @@ paymentListMember.addEventListener("click", (event) => {
   }
 });
 
+/* 오늘날짜로 세팅 */
+const inputElement = document.querySelector("#date-input");
+const date = new Date(); // 날짜 객체 생성
+const year = date.getFullYear();
+const month = (date.getMonth() + 1).toString().padStart(2, "0");
+const day = date.getDate().toString().padStart(2, "0"); 
+const formattedDate = `${year}${month}${day}`;
+inputElement.value = formattedDate;
