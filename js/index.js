97aeab4e706927e3a303de8d6e22a247bcde0318
JH <vvaa277@gmail.com>
2023-03-10 15:19:23 +0900

    Feat: 금액 입력 세자리마다 콤마 찍는 기능 추가

--- js/index.js
+++ js/index.js
@@ -5,27 +5,15 @@ const $All = (selector) => document.querySelectorAll(selector);
 
 const tabButtons = document.querySelectorAll(".tab-button");
 const tabContents = document.querySelectorAll(".tab-content");
-
 const buttonPaths = document.querySelectorAll(".tab-button path");
 
-console.log(tabButtons)
-console.log(tabContents)
-
 tabButtons.forEach((button, index) => {
-  //탭버튼 모음 buttons = [button,button,button] 
-  // querySelectorAll = ".tab-button" 이 선택자(#이면 아이디, . 클래스) 를 가진 모든 요소의 배열[]
-  // querySelector = ".tab-button"을 가진 모든 요소의 첫번째 = buttons[0]
-
   button.addEventListener("click", function (event) {
-    //button하나 클릭시
     tabContents.forEach((tab) => {
-      //탭 영역 모음 tabContents = [tab,tab,tab]
       tab.classList.remove("active");
-      //모든 탭의 .acitve를 떼어줌(기본속성으로 세팅된 display: none으로 인해 감춰짐)
     });
 
     tabContents[index].classList.add("active");
-     //해당 탭에(=[index]) .acitve를 붙여주어 영역 보여주기(display: block or flex 등 none이아니면 됨)
 
     /* 버튼 색상 */
     buttonPaths.forEach((path) => {
@@ -49,7 +37,7 @@ const chartHidden = document.querySelectorAll(".hidden_area");
 // chartBoard와 chartHidden영역이 아닌곳을 클릭하면 다시 닫기?
 chartBoard.addEventListener("click", function () {
   chartHidden.forEach((element) => {
-    element.classList.remove("hidden"); 
+    element.classList.remove("hidden");
   });
 });
 
@@ -83,6 +71,21 @@ const inputElement = document.querySelector("#date-input");
 const date = new Date(); // 날짜 객체 생성
 const year = date.getFullYear();
 const month = (date.getMonth() + 1).toString().padStart(2, "0");
-const day = date.getDate().toString().padStart(2, "0"); 
+const day = date.getDate().toString().padStart(2, "0");
 const formattedDate = `${year}${month}${day}`;
 inputElement.value = formattedDate;
+
+/* 세자리마다 콤마 */
+const priceInput = document.querySelector("#price-input");
+priceInput.addEventListener("keyup", function (e) {
+  let value = e.target.value;
+  value = Number(value.replaceAll(",", ""));
+  if (isNaN(value)) {
+    //NaN인지 판별
+    priceInput.value = 0;
+  } else {
+    //NaN이 아닌 경우
+    const formatValue = value.toLocaleString("ko-KR");
+    priceInput.value = formatValue;
+  }
+});
