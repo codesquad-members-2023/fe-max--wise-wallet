652f69a531044d9a4144a36e739cd0a1cecfae9e
silvertae <massenger7@gmail.com>
2023-03-11 13:50:29 +0900

    Feat: 분류 항목도 커스텀 드롭다운 메뉴로 수정 후 그에 맞게 js파일 수정
    결제수단에만 적용되었던 드롭다운 메뉴를 분류 선택창에서도 추가했다.
    여러 드롭다운 메뉴를 하나의 js 파일로 관리하기 위해서 dropdown.js 파일을
    수정하였다.
    
    수정하는 과정에서 클래스 명도 select-payment-dropdown과 select-category
    -dropdown을 하나의 select-dropdown으로 묶어서 수정하였다.
    
    querySelectorAll과 html의 노드 트리 구조를 제대로 이해하는게
    어려운 것 같다.

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,24 +1,39 @@
-const dropdown = document.querySelector('.select-payment-dropdown');
-const inputbox = document.querySelector('.select-box');
-const options = document.querySelectorAll('.option-item');
-
-const handleSelect = item => {
-	dropdown.classList.remove('active');
-	inputbox.value = item.children[0].innerHTML;
+const inputbox = document.querySelectorAll('.select-box');
+// 인풋창을 눌렀을 때 드롭다운 메뉴 숨기고 선택한 항목 표시
+const handleSelect = (ib, item) => {
+	ib.parentNode.classList.remove('active');
+	ib.value = item.children[0].innerHTML;
 };
-// 드롭다운 메뉴 중 옵션 하나 선택시 닫으면서 인풋창에 텍스트 입력
-options.forEach(option => {
-	option.addEventListener('click', () => handleSelect(option));
-});
-// 선택창 클릭시 드롭다운 열어주기
-inputbox.addEventListener('click', () => {
-	dropdown.classList.toggle('active');
+// 선택된 드롭다운 메뉴에서 옵션들을 불러온 후
+// 각 옵션들이 선택되었을 때 handleSelect 함수 실행
+const clickInputBox = (ib, optionItems) => {
+	if (ib.parentNode.classList.contains('active')) {
+		ib.parentNode.classList.remove('active');
+		optionItems.forEach(opt => {
+			opt.removeEventListener('click', () => handleSelect(ib, opt));
+		});
+	} else {
+		ib.parentNode.classList.add('active');
+		optionItems.forEach(opt => {
+			opt.addEventListener('click', () => handleSelect(ib, opt));
+		});
+	}
+};
+// 여러 드롭다운 메뉴가 있을 때 인풋창 클릭된 드롭다운 메뉴의
+// 옵션 항목들을 가져온 후 clickInputBox 함수 실행
+inputbox.forEach(ib => {
+	ib.addEventListener('click', () => {
+		let optionList = ib.nextElementSibling;
+		let optionItems = optionList.querySelectorAll('.option-item');
+		clickInputBox(ib, optionItems);
+	});
 });
 // 선택창 외 다른 곳 클릭시 드롭다운 메뉴를 닫아주는 함수
 const handleClose = e => {
 	if (!e.target.classList.contains('select-box') && !e.target.classList.contains('option-item')) {
-		dropdown.classList.remove('active');
+		inputbox.forEach(ib => {
+			ib.parentNode.classList.remove('active');
+		});
 	}
 };
-
 window.addEventListener('click', e => handleClose(e));
