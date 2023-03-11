61944a93a873671d7d51cf4fc970183d84d53785
silvertae <massenger7@gmail.com>
2023-03-11 11:46:24 +0900

    Feat: 드롭다운 DOM event를 onclick에서 addEventListener로 수정
    기존에 onclick() 방식으로 만들었던 dropdown.js를 요구사항에 맞추기 위해서
    addEventListener를 사용한 DOM Event로 수정하였다.

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,10 +1,25 @@
-// const dropdown = document.querySelector('.select-payment-dropdown');
+const dropdown = document.querySelector('.select-payment-dropdown');
+const inputbox = document.querySelector('.select-box');
+const options = document.querySelectorAll('.option-item');
 
-function show(option) {
-	if (option) document.querySelector('.select-box').value = option;
+const handleSelect = item => {
+	dropdown.classList.remove('active');
+	inputbox.value = item.children[0].innerHTML;
+};
 
-	let dropdown = document.querySelector('.select-payment-dropdown');
-	dropdown.onclick = () => {
-		dropdown.classList.toggle('active');
-	};
-}
+options.forEach(option => {
+	option.addEventListener('click', () => handleSelect(option));
+});
+
+inputbox.addEventListener('click', () => {
+	dropdown.classList.toggle('active');
+});
+
+// function show(option) {
+// 	if (option) document.querySelector('.select-box').value = option;
+
+// 	let dropdown = document.querySelector('.select-payment-dropdown');
+// 	dropdown.onclick = () => {
+// 		dropdown.classList.toggle('active');
+// 	};
+// }
