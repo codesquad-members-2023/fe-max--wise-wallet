802ec585290c74f5fa20b168740bb176447870a6
silvertae <massenger7@gmail.com>
2023-03-11 12:32:01 +0900

    Feat: 드롭다운 선택창 외 다른 영역 선택시 메뉴를 닫는 기능 추가
    인풋바의 결제수단 선택창에서 드롭다운 메뉴외 화면의 다른 영역을 클릭했을
    때도 드롭다운 메뉴를 닫아서 안보이게 해주는 함수를 dropdown.js 파일에
    추가하였다.

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -6,20 +6,19 @@ const handleSelect = item => {
 	dropdown.classList.remove('active');
 	inputbox.value = item.children[0].innerHTML;
 };
-
+// 드롭다운 메뉴 중 옵션 하나 선택시 닫으면서 인풋창에 텍스트 입력
 options.forEach(option => {
 	option.addEventListener('click', () => handleSelect(option));
 });
-
+// 선택창 클릭시 드롭다운 열어주기
 inputbox.addEventListener('click', () => {
 	dropdown.classList.toggle('active');
 });
+// 선택창 외 다른 곳 클릭시 드롭다운 메뉴를 닫아주는 함수
+const handleClose = e => {
+	if (!e.target.classList.contains('select-box') && !e.target.classList.contains('option-item')) {
+		dropdown.classList.remove('active');
+	}
+};
 
-// function show(option) {
-// 	if (option) document.querySelector('.select-box').value = option;
-
-// 	let dropdown = document.querySelector('.select-payment-dropdown');
-// 	dropdown.onclick = () => {
-// 		dropdown.classList.toggle('active');
-// 	};
-// }
+window.addEventListener('click', e => handleClose(e));
