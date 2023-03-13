3b2f2234d7f3999fe5cf7870b7a508462bd19614
jay <chlwodnjs34@gmail.com>
2023-03-14 00:30:35 +0900

    [fix] input bar 입력 후 체크버튼 활성화 버그 수정

--- App/src/js/localStorage/resetInputBar.js
+++ App/src/js/localStorage/resetInputBar.js
@@ -1,4 +1,5 @@
 import { setNow } from "../date/setNow.js";
+import { changeCategoryOption } from "../inputBar/changeCategoryOption.js";
 
 export const resetInputBar = (
   date,
@@ -9,15 +10,25 @@ export const resetInputBar = (
   category_select
 ) => {
   setNow();
+  if (isPositive.checked) {
+    changeCategoryOption();
+  }
+
   price.value = "";
-  isPositive.checked = false;
   content.value = "";
   payment.value = "";
   category_select.value = "";
+  isPositive.checked = false;
 
   const $selected_value = document.querySelectorAll(".selected-value");
+  const $input = document.querySelector("#input_bar input");
 
   $selected_value.forEach((div) => {
     div.textContent = "선택하세요";
   });
+
+  // 이벤트 객체를 생성
+  const changeEvent = new Event("change");
+  // input에 직접 change 이벤트를 발생
+  $input.dispatchEvent(changeEvent);
 };
--- App/src/js/main/removeList.js
+++ App/src/js/main/removeList.js
@@ -1,6 +1,5 @@
 export const removeList = () => {
   const $main_history_list = document.querySelector("#main_history_list");
-
   while ($main_history_list.firstChild) {
     $main_history_list.removeChild($main_history_list.firstChild);
   }
