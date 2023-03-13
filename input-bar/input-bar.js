905974becb383316c0baaaf091f1a613654f9996
bakhacode <bakhamitai@naver.com>
2023-03-14 02:37:12 +0900

    Feat: 인풋바 memo 부분 기능 구현
    memo-input 태그에 영역을 클릭하면 힌트 문구가 사라지면서 프라이머리 컬러의 텍스트를 입력할 수 있다.
    입력한 내용을 지우고 다른 영역을 클릭해서 blur 처리되면 원래의 힌트 문구가 나온다.

--- input-bar/input-bar.js
+++ input-bar/input-bar.js
@@ -21,21 +21,18 @@ dateInput.addEventListener("input", function () {
     }
 });
 
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
 // const categoryPriceInput = document.getElementById("category-price");
 
-// categoryPriceInput.addEventListener("input", function () {
-//     const inputValue = this.value;
-//     if (inputValue && !/^[0-9]*$/.test(inputValue)) {
-//         this.value = inputValue.replace(/[^0-9]/g, "");
-//     }
-//     if (this.value.length > 8) {
-//         this.value = this.value.slice(0, 8);
-//     }
+// categoryPriceInput.addEventListener("focus", function () {
+//     this.placeholder = "";
+// });
 
-//     // 수정된 부분 시작
-//     const parsedValue = parseInt(this.value.replace(/,/g, ""));
-//     this.value = isNaN(parsedValue) ? 0 : parsedValue.toLocaleString();
-//     // 수정된 부분 끝
+// categoryPriceInput.addEventListener("blur", function () {
+//     if (this.value === "") {
+//         this.placeholder = "0";
+//     }
 // });
 
 const categoryPriceInput = document.getElementById("category-price");
@@ -44,19 +41,41 @@ categoryPriceInput.addEventListener("input", function () {
     const inputValue = this.value;
     if (inputValue && !/^[0-9]*$/.test(inputValue)) {
         this.value = inputValue.replace(/[^0-9]/g, "");
+    } else if (this.value.length > 7) {
+        this.value = this.value.slice(0, 7);
+    } else {
+        this.value = Number(inputValue.replace(/[^0-9]/g, "")).toLocaleString();
     }
-    if (this.value.length > 8) {
-        this.value = this.value.slice(0, 8);
-    }
-
-    const parsedValue = parseInt(this.value.replace(/,/g, ""));
-    this.value = isNaN(parsedValue) ? 0 : parsedValue.toLocaleString();
 });
 
-// 수정된 부분 시작
-const categoryPricePlaceholder = categoryPriceInput.getAttribute("placeholder");
+categoryPriceInput.addEventListener("keydown", function (event) {
+    if (event.keyCode === 9 || event.keyCode === 13) {
+        event.preventDefault();
+    }
+});
+
+categoryPriceInput.addEventListener("focus", function () {
+    this.placeholder = "";
+});
+
 categoryPriceInput.addEventListener("blur", function () {
-    if (!this.value) {
-        this.value = categoryPricePlaceholder;
+    if (this.value === "") {
+        this.placeholder = "0";
+    }
+});
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+const memoInput = document.getElementById("memo-input");
+
+// input 영역 클릭 시 placeholder 내용 숨기기
+memoInput.addEventListener("focus", function () {
+    this.placeholder = "";
+});
+
+// 다른 영역 클릭 시 placeholder 내용 보이기
+memoInput.addEventListener("blur", function () {
+    if (this.value === "") {
+        this.placeholder = "입력하세요";
     }
 });
