ea76fef160e0db3716a776498317d685cc48866e
jay <chlwodnjs34@gmail.com>
2023-03-15 02:57:33 +0900

    [feat] localstorage 값 정렬후 return

--- App/src/js/localStorage/getLocalStorage.js
+++ App/src/js/localStorage/getLocalStorage.js
@@ -2,5 +2,5 @@ export const getLocalStorage = () => {
   const keys = Object.keys(localStorage);
   const values = keys.map((key) => JSON.parse(localStorage.getItem(key)));
 
-  return values;
+  return values.sort((a, b) => Number(b.uniqueKey) - Number(a.uniqueKey));
 };
