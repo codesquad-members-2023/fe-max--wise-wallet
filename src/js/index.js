9b1114d109f6af2d12c38632c525e0db1a329273
silvertae <massenger7@gmail.com>
2023-03-15 02:09:42 +0900

    Feat: 인풋바 카테고리 선택창에 수입/지출 분류 전환 기능 추가
    금액 입력칸의 +,- 체크박스가 체크된 상태(+, 수입)인지 체크가 해제된 상태
    (-, 지출)인지에 따라서 카테고리 드롭다운 메뉴의 리스트가 다르게 보이도록
    html과 js 코드를 추가했다.
    
    인라인 스타일로 display 속성을 조작해서 만들었다. 다만 이렇게 했을 시
    수입일 경우 리스트의 옵션을 클릭해도 값이 제대로 들어가지 않는 문제가
    발생해서 dropdown.js 파일을 수정해야 할 것 같다.

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import { initInputDate } from './initInputDate.js';
 import './dropdown.js';
 import './formValidation.js';
 import './inputPrice.js';
+import './switchCategory.js';
 
 const init = () => {
 	initInputDate();
