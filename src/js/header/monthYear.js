442c08808649f3197ca1c16fb7fa9efa316a52a8
silvertae <massenger7@gmail.com>
2023-03-15 19:38:28 +0900

    Feat: 헤더 연월표시창 이전달 이동 기능 추가
    btnClickEventHandler()와 clickPrevBtnHandler() 함수를 만들어서
    왼쪽 화살표 버튼을 클릭했을 때 이전달로 표시창의 연월을 바꿔주는 기능을
    추가하였다.
    
    조건문을 사용해 1월에서 이전달로 넘어갈 때는 year에서 1을 빼주고 month는
    12가 되도록 만들었다.
    
    이 과정에서 연월 이름들이 들어있던 객체인 monthNameList가 재사용되어서
    파일로 따로 분리해 새로 만든 constant 폴더로 옮겨 import 해오도록
    변경했다.

--- src/js/header/monthYear.js
+++ src/js/header/monthYear.js
@@ -1,24 +1,10 @@
 import { getToday } from '../inputbar/getToday.js';
+import { monthNameList } from '../constant/monthNameList.js';
 
 function displayCurrentMonthYear() {
 	const { year, month } = getToday();
 	const currentYear = year;
 	const currentMonth = month;
-
-	const monthNameList = {
-		1: 'January',
-		2: 'Feburary',
-		3: 'March',
-		4: 'April',
-		5: 'May',
-		6: 'June',
-		7: 'July',
-		8: 'August',
-		9: 'September',
-		10: 'October',
-		11: 'November',
-		12: 'December',
-	};
 
 	const $currentYearEl = document.querySelector('div.current-month-year p:nth-of-type(1)');
 	const $currentMonthEl = document.querySelector('div.current-month-year p:nth-of-type(2)');
@@ -31,21 +17,37 @@ function displayCurrentMonthYear() {
 
 function initHeaderMonthYear() {
 	displayCurrentMonthYear();
-	// btnClickEventHandler();
+	btnClickEventHandler();
 }
 
-// function btnClickEventHandler() {
-//   이전, 다음 버튼 요소 불러오기
-// 	각 버튼에 이벤트 걸기
-//   이전 버튼 클릭 -> clickPrevBtnHandler();
-// 	다음 버튼 클릭 -> clickNextBtnHandler();
-// }
+function btnClickEventHandler() {
+	const $prevBtnEl = document.querySelector('.prev-btn');
+	const $nextBtnEl = document.querySelector('.next-btn');
 
-// function clickPrevBtnHandler() {
-//   연월 요소 불러오기
-//   연 수정 (선택)
-//   월 -1 수정
-// }
+	$prevBtnEl.addEventListener('click', clickPrevBtnHandler);
+	// $nextBtnEl.addEventListener('click', clickNextBtnHandler);
+}
+
+function clickPrevBtnHandler() {
+	const $currentYearEl = document.querySelector('div.current-month-year p:nth-of-type(1)');
+	const $currentMonthEl = document.querySelector('div.current-month-year p:nth-of-type(2)');
+	const $currentMonthNameEl = document.querySelector('div.current-month-year p:nth-of-type(3)');
+
+	let year = Number($currentYearEl.innerText);
+	let month = Number($currentMonthEl.innerText);
+
+	if (month === 1) {
+		year -= 1;
+		month = 12;
+		$currentYearEl.innerText = year;
+		$currentMonthEl.innerText = month;
+		$currentMonthNameEl.innerText = monthNameList[month];
+	} else {
+		month -= 1;
+		$currentMonthEl.innerText = month;
+		$currentMonthNameEl.innerText = monthNameList[month];
+	}
+}
 
 // function clickNextBtnHandler() {
 //   연월 요소 불러오기
