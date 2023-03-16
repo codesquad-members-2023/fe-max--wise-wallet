0ded2842776f67eefa5c16e6107e06edbf30a6f9
wonkyung <mwk0725@gmail.com>
2023-03-16 20:58:16 +0900

    feat: 드롭 다운 토글 기능 추가

---
+++ src/js/historyInput/Dropbox.js
@@ -0,0 +1,32 @@
+import { $$ } from '../utils/dom.js';
+
+const dropdowns = $$('.dropdown__wrap');
+
+const toggleDropdown = (e) => {
+  e.stopPropagation();
+  e.currentTarget.classList.toggle('opened');
+};
+
+const selectOption = (e) => {
+  const input = e.currentTarget
+    .closest('.dropdown__wrap')
+    .querySelector('input');
+  input.value = e.currentTarget.textContent.trim();
+};
+
+const closeDropdownFromOutside = () => {
+  dropdowns.forEach((dropdown) => {
+    if (dropdown.classList.contains('opened')) {
+      dropdown.classList.remove('opened');
+    }
+  });
+};
+
+document.body.addEventListener('click', closeDropdownFromOutside);
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener('click', toggleDropdown);
+  dropdown.querySelectorAll('.option').forEach((option) => {
+    option.addEventListener('click', selectOption);
+  });
+});
