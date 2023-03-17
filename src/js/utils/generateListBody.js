fd01726ad43a8529b5b9d9b9ca2d6ad7529d2435
JH <vvaa277@gmail.com>
2023-03-17 15:34:43 +0900

    Feat: 리스트 렌더링 수정중..
    해당 날짜의 가장 뒤에 작은 li를 붙이고 날짜가 달라지면 큰 li가 새로 생기는 방식으로 진행중이나 잘 되지 않는 중

--- src/js/utils/generateListBody.js
+++ src/js/utils/generateListBody.js
@@ -1,28 +1,31 @@
-import { generateListDetail } from "./generateListDetail.js";
+import { $ } from "./dom.js";
+import { inputStore } from "../store/inputStore.js";
 
 export const generateListBody = () => {
+  const listArray = inputStore.listArray;
   document.querySelector(".list-by-day-box").insertAdjacentHTML(
     "beforeend",
     `
-        <li class="list-by-day">
-          <div class="list-body">
-            <div class="list-body-info">
-              <div class="date-day">
-                <span class="date-day-date">2월 10일</span>
-                <span class="date-day-day">금</span>
-              </div>
-              <div class="income-expenditure">
-                <span>수입</span>
-                <span>2,010,580원</span>
-                <span>지출</span>
-                <span>9,500원</span>
-              </div>
+    <li class="list-by-day">
+        <div class="list-body">
+        <div class="list-body-info">
+            <div class="date-day">
+            <span class="date-day-date">${
+              $(".header-month").textContent
+            }월 ${listArray[listArray.length - 1].date.slice(-2)}일</span>
+            <span class="date-day-day">금</span>
             </div>
-            <ul class="list-datail-box">
-              ${generateListDetail()}
-            </ul>
-          </div>
-        </li>
-        `
+            <div class="income-expenditure">
+            <span>수입</span>
+            <span>2,010,580원</span>
+            <span>지출</span>
+            <span>9,500원</span>
+            </div>
+        </div>
+        <ul class="list-datail-box">
+        </ul>
+        </div>
+    </li>
+    `
   );
 };
--- src/js/utils/renderListByDay.js
+++ src/js/utils/renderListByDay.js
@@ -14,144 +14,150 @@ import { generateListDetail } from "./generateListDetail.js";
 //   categoryIn: categoryIn,
 // };
 export const renderListByDay = () => {
+  const listArray = inputStore.listArray;
+
   // 해당 월의 수입 지출 현황
   if ($("#income-btn").checked) {
 
   }
   if ($("#expenditure-btn").checked) {
     // 총 건수 변화하는 함수
-    const listArray = inputStore.listArray;
-    if (inputStore.listArray.length === 1) {
-      // generateListBody();
-      document.querySelector(".list-by-day-box").innerHTML = `
-        <li class="list-by-day">
-          <div class="list-body">
-            <div class="list-body-info">
-              <div class="date-day">
-                <span class="date-day-date">2월 10일</span>
-                <span class="date-day-day">금</span>
-              </div>
-              <div class="income-expenditure">
-                <span>수입</span>
-                <span>2,010,580원</span>
-                <span>지출</span>
-                <span>9,500원</span>
-              </div>
-            </div>
-            <ul class="list-datail-box">
-            </ul>
-          </div>
-        </li>
-      `;
-      document.querySelector(".list-datail-box").insertAdjacentHTML(
-        "beforeend",
-        `
-          <li>
-            <div class="list-detail">
-                <div class="list-detail-category">
-                    <span>${
-                      listArray[listArray.length - 1].categoryIn
-                    }</span>
-                </div>
-                <span class="list-detail-body ">${
-                  listArray[listArray.length - 1].memo
-                }</span>
-                <span class="list-detail-payment list-detail-item">${
-                  listArray[listArray.length - 1].paymentIn
-                }</span>
-                <span class="list-detail-price list-detail-item">-${
-                  listArray[listArray.length - 1].price
-                }원</span>
-                <button title="삭제하기" class="delete-btn">
-                    <img src="./src/images/delete-x.svg" alt="삭제">
-                    <span class="delete-text">삭제하기</span>
-                </button>
-            </div>
-          </li>
-        `
-      );
+    console.log(listArray.length)
+    console.log(
+      listArray.filter(el => el.date.slice(-2) === $('#date-input').value.slice(-2))
+    )
+    if (listArray.length === 1) {
+      generateListBody();
+      generateListDetail();
+      // document.querySelector(".list-by-day-box").innerHTML = `
+      //   <li class="list-by-day">
+      //     <div class="list-body">
+      //       <div class="list-body-info">
+      //         <div class="date-day">
+      //           <span class="date-day-date">2월 10일</span>
+      //           <span class="date-day-day">금</span>
+      //         </div>
+      //         <div class="income-expenditure">
+      //           <span>수입</span>
+      //           <span>2,010,580원</span>
+      //           <span>지출</span>
+      //           <span>9,500원</span>
+      //         </div>
+      //       </div>
+      //       <ul class="list-datail-box">
+      //       </ul>
+      //     </div>
+      //   </li>
+      // `;
+      // document.querySelector(".list-datail-box").insertAdjacentHTML(
+      //   "beforeend",
+      //   `
+      //     <li>
+      //       <div class="list-detail">
+      //           <div class="list-detail-category">
+      //               <span>${
+      //                 listArray[listArray.length - 1].categoryIn
+      //               }</span>
+      //           </div>
+      //           <span class="list-detail-body ">${
+      //             listArray[listArray.length - 1].memo
+      //           }</span>
+      //           <span class="list-detail-payment list-detail-item">${
+      //             listArray[listArray.length - 1].paymentIn
+      //           }</span>
+      //           <span class="list-detail-price list-detail-item">-${
+      //             listArray[listArray.length - 1].price
+      //           }원</span>
+      //           <button title="삭제하기" class="delete-btn">
+      //               <img src="./src/images/delete-x.svg" alt="삭제">
+      //               <span class="delete-text">삭제하기</span>
+      //           </button>
+      //       </div>
+      //     </li>
+      //   `
+      // );
     } else if (
-      inputStore.listArray.length > 1 &&
-      inputStore.listArray[inputStore.listArray.length - 2].date !==
-        inputStore.listArray[inputStore.listArray.length - 1].date
+      listArray.length > 1 &&
+      listArray.filter(el => el.date.slice(-2) === $('#date-input').value.slice(-2)).length === 1
     ) {
-      // generateListBody();
-      document.querySelector(".list-by-day-box").innerHTML = `
-        <li class="list-by-day">
-          <div class="list-body">
-            <div class="list-body-info">
-              <div class="date-day">
-                <span class="date-day-date">2월 10일</span>
-                <span class="date-day-day">금</span>
-              </div>
-              <div class="income-expenditure">
-                <span>수입</span>
-                <span>2,010,580원</span>
-                <span>지출</span>
-                <span>9,500원</span>
-              </div>
-            </div>
-            <ul class="list-datail-box">
-            </ul>
-          </div>
-        </li>
-        `;
-      document.querySelector(".list-datail-box").insertAdjacentHTML(
-        "beforeend",
-        `
-          <li>
-            <div class="list-detail">
-                <div class="list-detail-category">
-                    <span>${
-                      listArray[listArray.length - 1].categoryIn
-                    }</span>
-                </div>
-                <span class="list-detail-body ">${
-                  listArray[listArray.length - 1].memo
-                }</span>
-                <span class="list-detail-payment list-detail-item">${
-                  listArray[listArray.length - 1].paymentIn
-                }</span>
-                <span class="list-detail-price list-detail-item">-${
-                  listArray[listArray.length - 1].price
-                }원</span>
-                <button title="삭제하기" class="delete-btn">
-                    <img src="./src/images/delete-x.svg" alt="삭제">
-                    <span class="delete-text">삭제하기</span>
-                </button>
-            </div>
-          </li>
-        `
-      );
+      generateListBody();
+      generateListDetail();
+      // document.querySelector(".list-by-day-box").innerHTML = `
+      //   <li class="list-by-day">
+      //     <div class="list-body">
+      //       <div class="list-body-info">
+      //         <div class="date-day">
+      //           <span class="date-day-date">2월 10일</span>
+      //           <span class="date-day-day">금</span>
+      //         </div>
+      //         <div class="income-expenditure">
+      //           <span>수입</span>
+      //           <span>2,010,580원</span>
+      //           <span>지출</span>
+      //           <span>9,500원</span>
+      //         </div>
+      //       </div>
+      //       <ul class="list-datail-box">
+      //       </ul>
+      //     </div>
+      //   </li>
+      //   `;
+      // document.querySelector(".list-datail-box").insertAdjacentHTML(
+      //   "beforeend",
+      //   `
+      //     <li>
+      //       <div class="list-detail">
+      //           <div class="list-detail-category">
+      //               <span>${
+      //                 listArray[listArray.length - 1].categoryIn
+      //               }</span>
+      //           </div>
+      //           <span class="list-detail-body ">${
+      //             listArray[listArray.length - 1].memo
+      //           }</span>
+      //           <span class="list-detail-payment list-detail-item">${
+      //             listArray[listArray.length - 1].paymentIn
+      //           }</span>
+      //           <span class="list-detail-price list-detail-item">-${
+      //             listArray[listArray.length - 1].price
+      //           }원</span>
+      //           <button title="삭제하기" class="delete-btn">
+      //               <img src="./src/images/delete-x.svg" alt="삭제">
+      //               <span class="delete-text">삭제하기</span>
+      //           </button>
+      //       </div>
+      //     </li>
+      //   `
+      // );
     } else {
-      // generateListDetail();
-      document.querySelector(".list-datail-box").insertAdjacentHTML(
-        "beforeend",
-        `
-          <li>
-            <div class="list-detail">
-                <div class="list-detail-category">
-                    <span>${
-                      listArray[listArray.length - 1].categoryIn
-                    }</span>
-                </div>
-                <span class="list-detail-body ">${
-                  listArray[listArray.length - 1].memo
-                }</span>
-                <span class="list-detail-payment list-detail-item">${
-                  listArray[listArray.length - 1].paymentIn
-                }</span>
-                <span class="list-detail-price list-detail-item">-${
-                  listArray[listArray.length - 1].price
-                }원</span>
-                <button title="삭제하기" class="delete-btn">
-                    <img src="./src/images/delete-x.svg" alt="삭제">
-                    <span class="delete-text">삭제하기</span>
-                </button>
-            </div>
-          </li>
-        `
-      );
+      generateListDetail();
+      // document.querySelector(".list-datail-box").insertAdjacentHTML(
+      //   "beforeend",
+      //   `
+      //     <li>
+      //       <div class="list-detail">
+      //           <div class="list-detail-category">
+      //               <span>${
+      //                 listArray[listArray.length - 1].categoryIn
+      //               }</span>
+      //           </div>
+      //           <span class="list-detail-body ">${
+      //             listArray[listArray.length - 1].memo
+      //           }</span>
+      //           <span class="list-detail-payment list-detail-item">${
+      //             listArray[listArray.length - 1].paymentIn
+      //           }</span>
+      //           <span class="list-detail-price list-detail-item">-${
+      //             listArray[listArray.length - 1].price
+      //           }원</span>
+      //           <button title="삭제하기" class="delete-btn">
+      //               <img src="./src/images/delete-x.svg" alt="삭제">
+      //               <span class="delete-text">삭제하기</span>
+      //           </button>
+      //       </div>
+      //     </li>
+      //   `
+      // );
     }
 
     //         지금 상태에서 4번줄 하단에 console.dir(btn)해보세요. undefined 나온다면
