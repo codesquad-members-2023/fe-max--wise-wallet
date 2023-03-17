71c76d80cbbd5830008a89a47c6028a48a1268a4
wonkyung <mwk0725@gmail.com>
2023-03-17 20:12:04 +0900

    feat: 금액 앞 버튼 클릭 시 수입/지출 카테고리 목록 변환

--- src/js/historyInput/Dropbox.js
+++ src/js/historyInput/Dropbox.js
@@ -1,7 +1,8 @@
-import { $$ } from '../utils/dom.js';
+import { $, $$ } from '../utils/dom.js';
 import { checkFormFilled } from './CheckForm.js';
 
 const dropdowns = $$('.dropdown__wrap');
+const amountButton = $('.history__form-amount button');
 
 const toggleDropdown = (e) => {
   e.stopPropagation();
@@ -24,11 +25,40 @@ const closeDropdownFromOutside = () => {
   });
 };
 
-document.body.addEventListener('click', closeDropdownFromOutside);
-
-dropdowns.forEach((dropdown) => {
-  dropdown.addEventListener('click', toggleDropdown);
-  dropdown.querySelectorAll('.option').forEach((option) => {
-    option.addEventListener('click', selectOption);
+const setDropdownListeners = () => {
+  dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener('click', toggleDropdown);
+    dropdown.querySelectorAll('.option').forEach((option) => {
+      option.addEventListener('click', selectOption);
+    });
   });
-});
+};
+
+const changeCategoryList = () => {
+  const buttonImg = $('.history__form-amount button img');
+  const optionsList = $('.history__form-category ul');
+  const categoryInput = $('.history__form-category input');
+  const incomeCategoryList = optionsList.innerHTML;
+  const expenditureCategoryList = `
+  <li class="option">월급</li>
+  <li class="option">용돈</li>
+  <li class="option">기타수입</li>
+`;
+
+  categoryInput.value = '';
+
+  if (
+    buttonImg.attributes.src.value === './src/assets/button_spending_minus.svg'
+  ) {
+    optionsList.innerHTML = expenditureCategoryList;
+    buttonImg.attributes.src.value = './src/assets/button_spending_plus.svg';
+  } else {
+    optionsList.innerHTML = incomeCategoryList;
+    buttonImg.attributes.src.value = './src/assets/button_spending_minus.svg';
+  }
+  setDropdownListeners();
+};
+
+setDropdownListeners();
+document.body.addEventListener('click', closeDropdownFromOutside);
+amountButton.addEventListener('click', changeCategoryList);
