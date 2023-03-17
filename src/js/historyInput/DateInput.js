f2a1bc9815818ab038092638374fbc6a8ab94d9b
wonkyung <mwk0725@gmail.com>
2023-03-17 11:35:28 +0900

    fix: 일자 인풋 날짜 형식 수정
    - 월이 한 달 전으로 나오고 있었고, 20230317 형식이어야 하는데 2023317로 나오고 있어서 수정했습니다.

--- src/js/historyInput/DateInput.js
+++ src/js/historyInput/DateInput.js
@@ -3,5 +3,8 @@ import { year, month, date } from '../utils/date.js';
 
 export function initDateInput() {
   const dateInput = $('.history__form-date input');
-  dateInput.value = `${year}${month}${date}`;
+  const currMonth = String(month + 1).padStart(2, '0');
+  const currDate = String(date).padStart(2, '0');
+
+  dateInput.value = `${year}${currMonth}${currDate}`;
 }
