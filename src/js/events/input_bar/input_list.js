c107c4c2651d8e41d141c3b98e65d99793b49c50
박유진 <park@TOKO-MacBookAir.local>
2023-03-19 02:13:31 +0900

    Feat : 메인페이지 input bar의 payment 부분 이벤트 추가
    메인페이지 input bar의 payment 부분에서 category와 마찬가지로 input창을 클릭 했을때 child가 생성되고 보여지도록 이벤트를 추가한다

--- src/js/events/input_bar/input_list.js
+++ src/js/events/input_bar/input_list.js
@@ -1,6 +1,7 @@
 const select = {
   minusCategories : ['생활', '식비', '교통', '쇼핑/뷰티', '의료/건강', '문화/여가', '미분류'],
   plusCategories : ['월급', '용돈', '기타수입'],
+  paymentlist: ['현찰', '체크카드', '신용카드'],
   paymentInput : document.getElementById("payment_input"),
   paymentDiv: document.getElementById("payment_div"),
   categoryInput: document.getElementById("category_input"),
@@ -12,6 +13,18 @@ const select = {
 
   hideList(list) {
     list.style.display = "none";
+  },
+
+  addPayment() {
+    const newPaymentDiv = document.createElement("div");
+    this.paymentlist.forEach((value) => {
+      const textPaymentDiv = document.createElement("div");
+      const paymentTextNode = document.createTextNode(value);
+      textPaymentDiv.appendChild(paymentTextNode);
+      newPaymentDiv.appendChild(textPaymentDiv);
+      textPaymentDiv.id = "payment_list";
+    })
+    this.paymentDiv.appendChild(newPaymentDiv);
   },
 
   addList(value, newDiv) {
@@ -40,6 +53,8 @@ const select = {
 
 document.addEventListener("click", (e) => {
   if (e.target === select.paymentInput) {
+    select.paymentDiv.innerHTML = "";
+    select.addPayment();
     select.showList(select.paymentDiv);
   } else {
     select.hideList(select.paymentDiv);
