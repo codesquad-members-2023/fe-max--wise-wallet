33f8c23cea66feb41b09c41c4de4b864895af0f5
bakhacode <bakhamitai@naver.com>
2023-03-21 00:48:31 +0900

    Feat: month-year 부분 버블링을 이용한  기능 구현 복습
    DOM 조작에 익숙해지기 위해 지피티 참고용으로 구현했던 기능들을 멋지고 든든한 동료 Jay에게 도움을 얻어 구현했다.
    결제수단 분류 드롭다운을 구현할 때 사용했던 방식으로 중복되는 코드를 최대한 줄여서 구현했고, 버블링 개념을
    다시 한 번 익힐 수 있었다.

---
+++ header/header.js
@@ -0,0 +1,76 @@
+function getDateEl() {
+    const yearNumber = document.querySelector(".header__year");
+    const monthNumber = document.querySelector(".month-number");
+    const monthName = document.querySelector(".month-name");
+
+    return [yearNumber, monthNumber, monthName];
+}
+
+function setDate(dateElement, date) {
+    const [yearNumber, monthNumber, monthName] = dateElement;
+    yearNumber.textContent = date.getFullYear();
+    monthNumber.textContent = date.getMonth() + 1;
+    monthName.textContent = date.toLocaleString("en-US", {
+        month: "long",
+    });
+}
+setDate(getDateEl(), new Date());
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+// 헤더 먼스이어 안의 이미지태그로 이벤트리스너를 걸고
+// 안에서 클래스 prev next를 조건으로 걸어서 줄이고 늘리고 가능한지?
+// 챗지피티로 확인
+
+function monthIncrease(command) {
+    const num = command === "prev" ? 2 : 0;
+    const [yearNumber, monthNumber, monthName] = getDateEl();
+
+    const newDate = new Date(
+        yearNumber.textContent,
+        monthNumber.textContent - num
+    );
+    setDate([yearNumber, monthNumber, monthName], newDate);
+}
+
+const headerDate = document.querySelector(".header__month-year");
+
+headerDate.addEventListener("click", (e) => {
+    const $this = e.target;
+    if ($this.tagName === "IMG") {
+        $this.classList.contains("prev-month")
+            ? monthIncrease("prev")
+            : monthIncrease("next");
+    }
+});
+
+// nextMonth.addEventListener("click", () => {
+//     let currentMonth = parseInt(monthNumber.textContent);
+//     currentMonth++;
+//     if (currentMonth === 13) {
+//         currentMonth = 1;
+//     }
+//     monthNumber.textContent = currentMonth;
+// });
+
+// 이벤트 리스너에 함수를 넣어 중복되는 코드를 축약가능 [챗GPT]
+// function changeMonth(increment) {
+//     let currentMonth = parseInt(monthNumber.textContent);
+//     currentMonth += increment;
+//     if (currentMonth === 0) {
+//         currentMonth = 12;
+//     } else if (currentMonth === 13) {
+//         currentMonth = 1;
+//     }
+//     monthNumber.textContent = currentMonth;
+// }
+
+// prevMonth.addEventListener("click", () => {
+//     changeMonth(-1); // 이전 월 버튼의 경우, increment 값을 -1로 전달합니다.
+// });
+
+// nextMonth.addEventListener("click", () => {
+//     changeMonth(1); // 다음 월 버튼의 경우, increment 값을 1로 전달합니다.
+// });
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
