41865c21337be8ec66a1d867e04ab73d232aa3b2
Daeram Chung <daeram.chung@gmail.com>
2023-03-20 18:56:26 +0900

    Restructure top header components and add features
    - MonthYearCarousel
      - Add feature to change the current month.
      - Adapt logic from script file to the MonthYearCarousel component.
    - NavBar
      - Update tab image color upon navigating to another tab.
    - Move and adapt all styles from CSS files to each component.

--- src/components/top-header/TopHeader.js
+++ src/components/top-header/TopHeader.js
@@ -1,44 +1,53 @@
-import { routes } from "../../routes.js";
+const template = document.createElement("template");
+template.innerHTML = `
+  <header class="top-header">
+    <div class="top-header__inner-wrapper">
+      <div class="logo">
+        <img src="src/assets/icons/logo.svg" alt="Wise Wallet Logo" />
+      </div>
+
+      <month-year-carousel></month-year-carousel>
+
+      <nav-bar></nav-bar>
+    </div>
+  </header>
+
+  <style>
+    :host {
+      width: 100%;
+    }
+
+    .top-header {
+      width: 100%;
+      height: 194px;
+      display: flex;
+      justify-content: center;
+      background-color: var(--color-primary);
+      font-family: var(--ff-display);
+      color: var(--color-white);
+    }
+    
+    .top-header__inner-wrapper {
+      width: var(--layout-inner-width);
+      height: 96px;
+      margin-top: 40px;
+      padding-inline: 24px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    
+    .logo {
+      height: 24px;
+    }
+  </style>
+`;
 
 class TopHeader extends HTMLElement {
   constructor() {
     super();
-  }
-
-  navClickHandler(pathName) {
-    window.history.pushState({}, pathName, window.location.origin + pathName);
-    document.getElementById("root").innerHTML =
-      routes[pathName.replace("index.html", "")];
-  }
-
-  connectedCallback() {
-    this.render();
-
-    //- View Tabs
-    this.querySelectorAll("a").forEach((a) => {
-      a.addEventListener("click", (evt) => {
-        evt.preventDefault();
-        this.navClickHandler(a.dataset.path);
-      });
-    });
-  }
-
-  disconnectedCallback() {}
-
-  render() {
-    this.innerHTML = `
-      <header class="top-header">
-        <div class="top-header__inner-wrapper">
-          <div class="logo">
-            <img src="src/assets/icons/logo.svg" alt="Wise Wallet Logo" />
-          </div>
-      
-          <month-year-carousel></month-year-carousel>
-      
-          <nav-bar></nav-bar>
-        </div>
-      </header>
-    `;
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.append(template.content.cloneNode(true));
   }
 }
 
