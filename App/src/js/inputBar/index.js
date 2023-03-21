0019cca8898e5b7e9d1644cbffe7b4acba80a9a2
jay <chlwodnjs34@gmail.com>
2023-03-21 15:38:39 +0900

    [fix] customSelectInit 호출 위치 변경

--- App/src/js/inputBar/index.js
+++ App/src/js/inputBar/index.js
@@ -1,7 +1,9 @@
+import { customSelectInit } from "../customSelect/index.js";
 import { setDisplay } from "./display/setDisplay.js";
 import { setEvent } from "./event/setEvent.js";
 
 export const inputBarInit = () => {
   setDisplay();
   setEvent();
+  customSelectInit();
 };
