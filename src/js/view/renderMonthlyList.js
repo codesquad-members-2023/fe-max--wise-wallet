36063e9138dc9ed3e3d2fffa52748f2fbc0d6da0
JH <vvaa277@gmail.com>
2023-03-21 20:12:47 +0900

    feat: 주석제거중

--- src/js/view/renderMonthlyList.js
+++ src/js/view/renderMonthlyList.js
@@ -64,31 +64,4 @@ export const renderMonthlyList = () => {
   });
   document.querySelector('.list-by-day-box').innerHTML = htmlString
   
-
-  // document.querySelector(".list-by-day-box").insertAdjacentHTML(
-  //   "beforeend",
-  //   `
-  //   <li class="list-by-day">
-  //       <div class="list-body">
-  //       <div class="list-body-info">
-  //           <div class="date-day">
-  //           <span class="date-day-date">${$("#date-input").value.slice(
-  //             4,
-  //             6
-  //           )}월 ${$("#date-input").value.slice(-2)}일</span>
-  //           <span class="date-day-day">${getDayOfWeek()}</span>
-  //           </div>
-  //           <div class="income-expenditure">
-  //           <span>수입</span>
-  //           <span>2,010,580원</span>
-  //           <span>지출</span>
-  //           <span>9,500원</span>
-  //           </div>
-  //       </div>
-  //       <ul class="list-datail-box list-date-${sameDayArray[0].date}">
-  //       </ul>
-  //       </div>
-  //   </li>
-  //   `
-  // );
 };
