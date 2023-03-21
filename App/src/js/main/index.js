9cd6574b1dfb4cf0c38a39d87a033cffe88dcefa
jay <chlwodnjs34@gmail.com>
2023-03-21 15:37:56 +0900

    [fix] inputbar와setTodayDateInput 함수 호출 위치 변경

--- App/src/js/main/index.js
+++ App/src/js/main/index.js
@@ -1,7 +1,11 @@
+import { setTodayDateInput } from "../date/setTodayDateInput.js";
+import { inputBarInit } from "../inputBar/index.js";
 import { setMainDisplay } from "./display/setMainDisplay.js";
 import { setEvent } from "./event/setEvent.js";
 
 export const mainInit = () => {
+  inputBarInit();
   setMainDisplay();
   setEvent();
+  setTodayDateInput();
 };
