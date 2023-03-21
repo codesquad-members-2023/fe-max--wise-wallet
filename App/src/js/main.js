5fcbd3e20c84c125aab1eca8f38b479a1fa1f7f5
jay <chlwodnjs34@gmail.com>
2023-03-21 15:41:48 +0900

    [refact] 호출되는 함수 변경

--- App/src/js/main.js
+++ App/src/js/main.js
@@ -1,15 +1,9 @@
-import { customSelectInit } from "./customSelect/index.js";
-import { setTodayDateInput } from "./date/setTodayDateInput.js";
 import { headerInit } from "./header/index.js";
-import { inputBarInit } from "./inputBar/index.js";
 import { mainInit } from "./main/index.js";
 
 const init = () => {
   headerInit();
-  inputBarInit();
   mainInit();
-  setTodayDateInput();
-  customSelectInit();
 };
 
 window.addEventListener("DOMContentLoaded", init);
