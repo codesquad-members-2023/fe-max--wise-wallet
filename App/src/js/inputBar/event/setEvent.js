11f925e11f80945a92c6fce1ba781fb577f9d318
jay <chlwodnjs34@gmail.com>
2023-03-22 18:04:01 +0900

    [refact] 이벤트의 기능을 확인하 위한 주석 대신 의미를 알 수 있는 적절한 함수명을 사용해 분할

--- App/src/js/inputBar/event/setEvent.js
+++ App/src/js/inputBar/event/setEvent.js
@@ -9,12 +9,16 @@ import { resetInputBar } from "../display/resetInputBar.js";
 import { setMainDisplay } from "../../main/display/setMainDisplay.js";
 
 export const setEvent = () => {
-  const $date_input = document.getElementById("input_date");
-  const $price_toggle = document.getElementById("price_toggle");
-  const $input_price = document.getElementById("input_price");
+  clickSaveBtn();
+  checkDateFormat();
+  changeCategory();
+  formatCurrencyInput();
+  checkInputBarMode();
+};
+
+const clickSaveBtn = () => {
   const $input_checkbox = document.getElementById("input_checkbox");
 
-  // 저장 버튼
   $input_checkbox.addEventListener("click", (e) => {
     const $selected_list = document.querySelector(".selected_list");
     const key =
@@ -25,17 +29,29 @@ export const setEvent = () => {
     setMainDisplay();
     resetInputBar();
   });
-  // yyyymmdd 형식 체크
+};
+
+const checkDateFormat = () => {
+  const $date_input = document.getElementById("input_date");
   $date_input.addEventListener("blur", checkInputDate);
-  // 카테고리 분류 변경
+};
+
+const changeCategory = () => {
+  const $price_toggle = document.getElementById("price_toggle");
   $price_toggle.addEventListener("change", changeCategoryOption);
-  // 1000 단위 콤마 추가, 금액 숫자만 입력 가능
+};
+
+const formatCurrencyInput = () => {
+  const $input_price = document.getElementById("input_price");
   $input_price.addEventListener("input", (e) => {
     checkInputPrice(e);
     e.target.value = addComma(e.target.value);
   });
+};
+
+const checkInputBarMode = () => {
   const $input_bar = document.getElementById("input_bar");
-  // input bar data 변경
+
   $input_bar.addEventListener("keyup", (e) => {
     const $this = e.target;
     if ($this.tagName === "INPUT") {
