42e3b375bf14b2954eeb42ebbb1e4a79c97c988d
wonkyung <mwk0725@gmail.com>
2023-03-23 14:07:08 +0900

    refactor: 수입/지출 리스트 수정

--- src/js/historyList/List.js
+++ src/js/historyList/List.js
@@ -1,44 +1,53 @@
 import { $, getFormattedNumber } from '../utils/utils.js';
+import { getDayOfWeek, formatDate } from '../utils/date.js';
+import { formInput } from '../utils/elements.js';
 
-const dateInput = $('.history__form-date input');
-const amountInput = $('.history__form-amount input');
-const descriptionInput = $('.history__form-description input');
-const paymentMethodInput = $('.history__form-payment-method input');
-const categoryInput = $('.history__form-category input');
-const incomeOrExpenditureButton = $('.history__form-amount button img');
-const submitButton = $('.history__form-button');
+const $submitButton = $('.history__form-button');
+const $listWrap = $('.history-lists__wrap');
 
-const itemList = [];
+$submitButton.addEventListener('click', addItemToList);
 
-submitButton.addEventListener('click', (e) => {
+function addItemToList(e) {
   e.preventDefault();
-  const groupedData = getData();
-  addList(groupedData);
-  // 인풋 초기화 함수 추가하기
-});
+  const item = getData();
+  renderList(item);
+  resetInput();
+}
+
+function createItemList() {
+  const itemList = [];
+
+  function addItem(newItem) {
+    itemList.push(newItem);
+  }
+
+  function getItemList() {
+    return itemList;
+  }
+
+  return {
+    addItem,
+    getItemList,
+  };
+}
+
+const itemListManager = createItemList();
 
 function getData() {
   const now = new Date();
-  const id = now.getTime();
-  const date = dateInput.value;
-  const incomeOrExpenditure =
-    incomeOrExpenditureButton.getAttribute('data-value');
-  const amount = amountInput.value.replaceAll(',', '');
-  const description = descriptionInput.value;
-  const paymentMethod = paymentMethodInput.value;
-  const category = categoryInput.value;
+
   const newItem = {
-    id,
-    date,
-    incomeOrExpenditure,
-    amount,
-    description,
-    paymentMethod,
-    category,
+    id: now.getTime(),
+    date: formInput.$date.value,
+    incomeOrExpenditure: formInput.$amountIcon.getAttribute('data-value'),
+    amount: formInput.$amount.value.replaceAll(',', ''),
+    description: formInput.$description.value,
+    paymentMethod: formInput.$paymentMethod.value,
+    category: formInput.$category.value,
   };
 
-  itemList.push(newItem);
-  return makeGroupDate(itemList);
+  itemListManager.addItem(newItem);
+  return makeGroupDate(itemListManager.getItemList());
 }
 
 function makeGroupDate(list) {
@@ -54,83 +63,99 @@ function makeGroupDate(list) {
   return groupedData;
 }
 
-function addList(groupedData) {
-  const listWrap = document.querySelector('.history-lists__wrap');
-  listWrap.innerHTML = '';
+function createListItem(item) {
+  const { isIncome, isExpenditure } = getItemType(item);
 
-  Object.entries(groupedData).forEach(([dateData, listData]) => {
-    const dateMonth = Number(dateData.substring(4, 6));
-    const dateDay = dateData.substring(6, 8);
-    const dayOfWeek = getDayOfWeek(formatDate(dateData));
+  let amount = getFormattedNumber(`${item.amount}원`);
+  if (isExpenditure) amount = `-${amount}`;
 
-    let expenses = 0;
-    let incomes = 0;
+  const listItem = document.createElement('li');
+  const contentHTML = `
+    <div class="tag tag--life bold-medium">${item.category}</div>
+    <div class="history-list__item-content">
+      ${item.description}
+    </div>
+    <div class="history-list__item-payment-method">${item.paymentMethod}</div>
+    <div class="history-list__item-amount ${
+      isIncome ? 'plus-color' : 'minus-color'
+    }">
+      ${amount}원
+    </div>
+  `;
+  listItem.insertAdjacentHTML('beforeend', contentHTML);
 
-    const templateContentWrap = document.createElement('ul');
-    templateContentWrap.className = 'history-list__item body-medium';
+  return listItem;
+}
 
-    listData.forEach((item) => {
-      const isIncome = item.incomeOrExpenditure === 'income';
-      const isExpenditure = item.incomeOrExpenditure === 'expenditure';
+function createListTop(dateData, listData) {
+  const dateMonth = Number(dateData.substring(4, 6));
+  const dateDay = dateData.substring(6, 8);
+  const dayOfWeek = getDayOfWeek(formatDate(dateData));
 
-      if (isIncome) incomes += Number(item.amount);
-      if (isExpenditure) expenses -= Number(item.amount);
+  let expenses = 0;
+  let incomes = 0;
 
-      const listItem = document.createElement('li');
-      listItem.innerHTML = `
-        <div class="tag tag--life bold-medium">${item.category}</div>
-        <div class="history-list__item-content">
-          ${item.description}
+  listData.forEach((item) => {
+    const { isIncome, isExpenditure } = getItemType(item);
+
+    if (isIncome) incomes += Number(item.amount);
+    if (isExpenditure) expenses -= Number(item.amount);
+  });
+
+  const templateContentWrap = document.createElement('ul');
+  templateContentWrap.className = 'history-list__item body-medium';
+
+  listData.forEach((item) => {
+    const listItem = createListItem(item);
+    templateContentWrap.appendChild(listItem);
+  });
+
+  const templateListTop = `
+    <div class="history-list__top">
+      <div class="history-list__info bold-medium">
+        <div class="history-list__info-dateday">
+          <span class="history-list__info-date">${dateMonth}월 ${dateDay}일</span>
+          <span class="history-list__info-day">${dayOfWeek}</span>
         </div>
-        <div class="history-list__item-payment-method">${
-          item.paymentMethod
-        }</div>
-        <div class="history-list__item-amount plus-color">
-          ${getFormattedNumber(item.amount)}원
-        </div>
-      `;
-
-      templateContentWrap.appendChild(listItem);
-    });
-
-    const templateListTop = `
-      <div class="history-list__top">
-        <div class="history-list__info bold-medium">
-          <div class="history-list__info-dateday">
-            <span class="history-list__info-date">${dateMonth}월 ${dateDay}일</span>
-            <span class="history-list__info-day">${dayOfWeek}</span>
+        <div class="history-list-info__total">
+          <div class="history-list-info__expense">
+            지출 <span>${getFormattedNumber(String(expenses))}원</span>
           </div>
-          <div class="history-list-info__total">
-            <div class="history-list-info__expense">
-              수입 <span>${getFormattedNumber(String(incomes))}원</span>
-            </div>
-            <div class="history-list-info__income">
-              지출 <span>${getFormattedNumber(String(expenses))}원</span>
-            </div>
+          <div class="history-list-info__income">
+            수입 <span>${getFormattedNumber(String(incomes))}원</span>
           </div>
         </div>
       </div>
-    `;
+    </div>
+  `;
 
-    const listTop = document.createElement('div');
-    listTop.innerHTML = templateListTop + templateContentWrap.outerHTML;
+  const listTop = document.createElement('div');
+  listTop.insertAdjacentHTML('beforeend', templateListTop);
+  listTop.appendChild(templateContentWrap);
 
-    listWrap.appendChild(listTop);
+  return listTop;
+}
+
+function renderList(groupedData) {
+  clearList();
+
+  Object.entries(groupedData).forEach(([dateData, listData]) => {
+    const listTop = createListTop(dateData, listData);
+    $listWrap.appendChild(listTop);
   });
 }
 
-function getDayOfWeek(dateValue) {
-  const date = new Date(dateValue);
-  const dayOfWeek = date.getDay();
-  const days = ['일', '월', '화', '수', '목', '금', '토'];
+function getItemType(item) {
+  const isIncome = item.incomeOrExpenditure === 'income';
+  const isExpenditure = item.incomeOrExpenditure === 'expenditure';
 
-  return days[dayOfWeek];
+  return { isIncome, isExpenditure };
 }
 
-function formatDate(dateValue) {
-  const year = String(dateValue).slice(0, 4);
-  const month = String(dateValue).slice(4, 6);
-  const day = String(dateValue).slice(6, 8);
+function clearList() {
+  $listWrap.innerHTML = '';
+}
 
-  return `${year}-${month}-${day}`;
+function resetInput() {
+  formInput.$historyForm.reset();
 }
--- src/js/utils/date.js
+++ src/js/utils/date.js
@@ -21,3 +21,18 @@ export const monthTexts = [
 export const monthText = monthTexts[month - 1];
 
 export const getMonthText = (mon) => monthTexts[mon - 1];
+
+export const getDayOfWeek = (dateValue) => {
+  const dateInput = new Date(dateValue);
+  const dayOfWeek = dateInput.getDay();
+  const days = ['일', '월', '화', '수', '목', '금', '토'];
+
+  return days[dayOfWeek];
+};
+export const formatDate = (dateValue) => {
+  const yearStr = String(dateValue).slice(0, 4);
+  const monthStr = String(dateValue).slice(4, 6);
+  const dayStr = String(dateValue).slice(6, 8);
+
+  return `${yearStr}-${monthStr}-${dayStr}`;
+};
