5dcad478f98481d37ce454be494302bd17d23b67
jay <chlwodnjs34@gmail.com>
2023-03-23 23:14:35 +0900

    [refact]  변수의 일관성으 위해 로 변경

--- App/src/js/inputBar/display/setEditData.js
+++ App/src/js/inputBar/display/setEditData.js
@@ -2,30 +2,28 @@ import { getData } from "../../localStorage/getData.js";
 import { resetInputBar } from "./resetInputBar.js";
 import { setInputBar } from "./setInputBar.js";
 
-export const setEditData = (e) => {
-  const $this = e.target;
+export const setEditData = ({ target }) => {
+  if (!target.classList.contains("remove")) {
+    const $target_input_bar = target.closest("#input_bar");
+    const $target_dialog = target.closest("dialog");
+    const $list_tr = document.querySelector(".selected_list");
+    const isCancelSelect =
+      $target_input_bar === null &&
+      $list_tr !== null &&
+      $target_dialog === null;
 
-  if ($this.classList.contains("remove")) {
-    return;
-  }
+    if (isCancelSelect) {
+      $list_tr.classList.remove("selected_list");
+      resetInputBar();
+    }
 
-  const $target_input_bar = $this.closest("#input_bar");
-  const $target_Dialog = $this.closest("dialog");
-  const $list_tr = document.querySelector(".selected_list");
-  const isCancelSelect =
-    $target_input_bar === null && $list_tr !== null && $target_Dialog === null;
-
-  if (isCancelSelect) {
-    $list_tr.classList.remove("selected_list");
-    resetInputBar();
-  }
-
-  const $target_list_tr = $this.closest(".list_tr");
-  if ($target_list_tr !== null) {
-    resetInputBar();
-    $target_list_tr.classList.add("selected_list");
-    const key = $target_list_tr.querySelector(".uniqueKey").value;
-    const data = JSON.parse(getData(key));
-    setInputBar(data);
+    const $target_list_tr = target.closest(".list_tr");
+    if ($target_list_tr !== null) {
+      resetInputBar();
+      $target_list_tr.classList.add("selected_list");
+      const key = $target_list_tr.querySelector(".uniqueKey").value;
+      const data = JSON.parse(getData(key));
+      setInputBar(data);
+    }
   }
 };
